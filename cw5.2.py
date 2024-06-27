df216c2f14ffd09a9665dd573f7d5ba32e41a57e
MariaTalamaniuk <komaeda.kinnie228@gmail.com>
2024-06-27 18:20:30 +0100

    cw5.2

---
+++ cw5.2.py
@@ -0,0 +1,10 @@
+nested_dict = {
+    'person': {
+        'name': 'Alice',
+        'age': 25,
+        'contact': {
+            'email': 'alice@example.com',
+            'phone': '123-456-7890'
+        }
+    }
+}
