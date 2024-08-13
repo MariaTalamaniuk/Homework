9d0b10bf990615c69230d40dc1b56252e254151d
MariaTalamaniuk <komaeda.kinnie228@gmail.com>
2024-08-13 17:39:48 +0100

    hw1

--- hw1.py
+++ hw1.py
@@ -1,15 +1,14 @@
-# порівняти 3 числа і вивести  найбільше
-
 print('Задайте 3 цілі числа. ')
 
-number1 = input('Задайте 1 число: ')
-number2 = input('Задайте 2 число: ')
-number3 = input('Задайте 3 число: ')
+number1 = int(input('Задайте 1 число: '))
+number2 = int(input('Задайте 2 число: '))
+number3 = int(input('Задайте 3 число: '))
 
-max_number = number1
+max_number = number1  # Призначаємо перше число як початкове максимальне
 
-if number1 > max_number:
+if number2 > max_number:
     max_number = number2
 if number3 > max_number:
     max_number = number3
+
 print(f'Найбільше число: {max_number}')
